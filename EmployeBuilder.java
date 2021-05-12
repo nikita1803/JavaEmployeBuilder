d153c78033e987fdb40c6e53ca3214f757459f42
nikita1803 <rainikita595@gmail.com>
2021-05-12 12:01:57 +0530

    Added: Check the attendance of employe using random function

--- EmployeBuilder.java
+++ EmployeBuilder.java
@@ -3,6 +3,17 @@ package employeBuilderUC1;
 public class EmployeBuilder {
 	public static void main(String[] args) {
 		System.out.println("Welcome in employee wage");
+		
+		//UC-1 (EMPLOYEE ATTENDANCE)
+		
+		int isFullTime = 1;
+		double attendance = Math.floor(Math.random() * 10) % 2; 
+		if(attendance==1) {
+		  System.out.println("Employee is present");
+		}
+		else {
+			System.out.println("Employee is absent");
+		}
 	}
 
 }
