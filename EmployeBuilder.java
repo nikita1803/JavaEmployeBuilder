58bba665e58ab9a3f0f2c11440a9857819926e4b
nikita1803 <rainikita595@gmail.com>
2021-05-12 13:04:58 +0530

    Added: calculated daily employe wages

--- EmployeBuilder.java
+++ EmployeBuilder.java
@@ -4,16 +4,20 @@ public class EmployeBuilder {
 	public static void main(String[] args) {
 		System.out.println("Welcome in employee wage");
 		
-		//UC-1 (EMPLOYEE ATTENDANCE)
-		
 		int isFullTime = 1;
+		int wagePerHour = 20;
+		int workHours = 0;
 		double attendance = Math.floor(Math.random() * 10) % 2; 
 		if(attendance==1) {
 		  System.out.println("Employee is present");
+		  workHours = 8;
 		}
 		else {
 			System.out.println("Employee is absent");
 		}
+			int dailyWage = (wagePerHour * workHours);
+			System.out.println (dailyWage);
+		}
 	}
+	
 
-}
