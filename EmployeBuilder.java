6e0092ae71aa0b4c5717edccfaffedb8c90bcdb1
nikita1803 <rainikita595@gmail.com>
2021-05-18 01:12:40 +0530

    Added : Calculate total work hours of the employee

--- EmployeBuilder.java
+++ EmployeBuilder.java
@@ -6,28 +6,34 @@ public class EmployeBuilder
 	{
 		System.out.println("Welcome in employee wage");
 		
+		int hrsInMonth = 100;
 		int wagePerHour = 20;
+		int numWorkDays = 20;
+		int totalEmpHours = 0;
 		int workHours = 0;
 		int totalSalary = 0;
-		int numWorkDays = 20;
-		for( int day = 1; day <= numWorkDays; day++)
+		int workDays = 0;
+		
+		while(totalEmpHours <= hrsInMonth && workDays <= numWorkDays)
 		{
-		int attendance = (int) ((Math.random() * 10) % 3 + 1);
-		switch (attendance)
-		{
-			case 1: 
-				workHours = 8;
-			break;
-			case 2: 
-				workHours = 4;
-			break;
-			case 3: 
-				workHours = 0;
+			workDays++ ;
+			int attendance = (int) ((Math.random() * 10) % 3);
+			switch (attendance)
+			{
+				case 1: 
+					workHours = 8;
+				break;
+				case 2: 
+					workHours = 4;
+				break;
+				default: 
+					workHours = 0;
+			}
+			totalEmpHours = (workHours + totalEmpHours);
+			System.out.println("Total Working hours :" +totalEmpHours);
 		}
-		int salary = (workHours * wagePerHour);
-		totalSalary = (salary * numWorkDays);
-		}
-		System.out.println("Wages for a month :" +totalSalary);
+		totalSalary = (totalEmpHours * wagePerHour);
+		System.out.println("Total Wages for a working hours and days :" +totalSalary);
 	}
 }
 
