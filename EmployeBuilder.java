50570cb27093aac735869c3269e950cce7399fdd
nikita1803 <rainikita595@gmail.com>
2021-05-17 22:40:10 +0530

    Added: Calculated employee wages using switch case

--- EmployeBuilder.java
+++ EmployeBuilder.java
@@ -1,6 +1,7 @@
 package employeBuilderUC1;
 
-public class EmployeBuilder {
+public class EmployeBuilder 
+{
 	public static void main(String[] args) {
 		System.out.println("Welcome in employee wage");
 		
@@ -9,18 +10,21 @@ public class EmployeBuilder {
 		int wagePerHour = 20;
 		int workHours = 0;
 		int attendance = (int) ((Math.random() * 10) % 3);
-		switch (attendance){
-		case 1: workHours = 8;
-		break;
-		case 2: workHours = 4;
-		break;
-		case 3: workHours = 0;
+		switch (attendance)
+		{
+			case 1: 
+				workHours = 8;
+			break;
+			case 2: 
+				workHours = 4;
+			break;
+		    case 3: 
+		    	workHours = 0;
 		}
 			
-			int dailyWage = (wagePerHour * workHours);
-			System.out.println("Part time or Full time salray of employe :");
-			System.out.println (dailyWage);
+		int dailyWage = (wagePerHour * workHours);
+		System.out.println("Part time or Full time salray of employe :" +dailyWage);
 		}
-	}
+}
 	
 
