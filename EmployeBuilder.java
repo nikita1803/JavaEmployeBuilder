0032c5d4ba2dac7dabb7217bbedeb39cee52e719
nikita1803 <rainikita595@gmail.com>
2021-05-17 22:54:28 +0530

    Added: Caluclated monthly employee wages

--- EmployeBuilder.java
+++ EmployeBuilder.java
@@ -1,7 +1,9 @@
 package employeBuilderUC1;
 
-public class EmployeBuilder {
-	public static void main(String[] args) {
+public class EmployeBuilder 
+{
+	public static void main(String[] args) 
+	{
 		System.out.println("Welcome in employee wage");
 		
 		int isPartime = 1;
@@ -10,23 +12,26 @@ public class EmployeBuilder {
 		int workHours = 0;
 		int totalSalary = 0;
 		int numWorkDays = 20;
-		for( int day=1; day<=numWorkDays; day++)
+		for( int day = 1; day <= numWorkDays; day++)
 		{
 		int attendance = (int) ((Math.random() * 10) % 3);
-		switch (attendance){
-		case 1: workHours = 8;
-		break;
-		case 2: workHours = 4;
-		break;
-		case 3: workHours = 0;
+		switch (attendance)
+		{
+			case 1: 
+				workHours = 8;
+			break;
+			case 2: 
+				workHours = 4;
+			break;
+			case 3: 
+				workHours = 0;
 		}
-			int salary = (workHours * wagePerHour);
-			totalSalary = (salary * numWorkDays);
+		int salary = (workHours * wagePerHour);
+		totalSalary = (salary * numWorkDays);
 		}
-			System.out.println("Wages for a month :");
-			System.out.println (totalSalary);
-		}
+		System.out.println("Wages for a month :" +totalSalary);
 	}
+}
 
 	
 
