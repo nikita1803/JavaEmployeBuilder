1a6038eff9289e8da59ec859478ff688a010861e
nikita1803 <rainikita595@gmail.com>
2021-05-20 00:31:26 +0530

    Added: Refactor the Employee wage in procedural way

--- EmployeBuilder.java
+++ EmployeBuilder.java
@@ -1,73 +1,42 @@
 package employeBuilderUC1;
 import java.util.Scanner;
 public class EmployeBuilder 
-{
-		public String CompanyName;
-		public int MaxHours;
-		public int WagePerHour;
-		public int FullWorkHour;
-		public static int HalfWorkHour;
-		public int TotalWorkDays;
-		
-		public EmployeBuilder()
+{		
+	public final static int fullday = 1;
+	public final static int halfday = 2;
+	
+		public static int Attendance(String companyName , int MaxHours , int WagePerHour,int TotalWorkDays ) 
 		{
-			this.CompanyName = "";
-			this.MaxHours = 0;
-			this.WagePerHour = 0;
-			this.FullWorkHour = 0;
-			this.HalfWorkHour = 0;
-			this.TotalWorkDays = 0;
-		}
-		public static int Attendance(int MaxHours , int TotalWorkDays,int FullWorkHour,int HalfWaorkHour) 
-		{
-			int workDays = 0;
-			int workHours = 0;
+			int workHours = 0 ,empHours = 0 ,workDays = 0;
 			while(workHours <= MaxHours && workDays <= TotalWorkDays)
 			{
 				workDays++ ;	
 				int attendance = (int) Math.floor(Math.random() * 10) % 3; 
 				switch (attendance)
 				{
-					case 1: 
-						workHours = workHours+FullWorkHour;
+					case fullday: 
+						empHours = 8;
 					break;
-					case 2: 
-						workHours = workHours+HalfWorkHour;
+					case halfday: 
+						empHours = 4;
 					break;
 					default: 
-						workHours = workHours+0;
+						empHours = 0;
 				}
-				System.out.println("Total Working hours :" +workHours);
+				workHours += empHours;
+			
 			}
-				return workHours;
-		}
-		public static void CalculateWages (int WagePerHour, int workHours) 
-		{
+			System.out.println("Total working days" + workDays + "Total working hours" + workHours );
 			int totalSalary = ( WagePerHour * workHours);
 			System.out.println("Total Wages of the employee :" +totalSalary);
+			return totalSalary;
 		}
 		public static void main(String[] args)
 		{
 			System.out.println("Welcome in employee wage");
 			
-			EmployeBuilder emp = new EmployeBuilder();
-			Scanner sc = new Scanner(System.in);
-			System.out.println("Enter the company Name :");
-			emp.CompanyName = sc.nextLine();
-			sc.nextLine();
-			System.out.println("Enter the Full Work hour :");
-			emp.FullWorkHour = sc.nextInt();
-			System.out.println("Enter the half Work hour :");
-			emp.HalfWorkHour = sc.nextInt();
-			System.out.println("Enter the Maximum working hour :");
-			emp.MaxHours = sc.nextInt();
-			System.out.println("Enter the Wage per hour :");
-			emp.WagePerHour = sc.nextInt();
-			System.out.println("Enter the total work days :");
-			emp.TotalWorkDays = sc.nextInt();
-			
-			int totalworkhours = emp.Attendance(emp.MaxHours,emp.TotalWorkDays,emp.FullWorkHour,emp.HalfWorkHour);
-			emp.CalculateWages(emp.WagePerHour,totalworkhours);
+			Attendance("Honda", 50, 7, 10);
+			Attendance("Relience", 50, 6, 10);
 		}
 }
 
